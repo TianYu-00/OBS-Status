c5dc43889b72e83ad6d84dc23747baedad051657
ImNiYeYe <66271788+ImNiYeYe@users.noreply.github.com>
2020-06-26 15:10:39 +0100

    OBS Status 1.0.0.1

---
+++ OBS Status/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Integer) As Short
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start() 'Start timer
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (GetAsyncKeyState(33)) Then 'Page up key
+            Label1.Text = ("Recording")
+            PictureBox1.Image = My.Resources.red_status
+
+        ElseIf (GetAsyncKeyState(34)) Then 'Page down key
+            Label1.Text = ("Idle")
+            PictureBox1.Image = My.Resources.idle_status
+        End If
+    End Sub
+End Class
